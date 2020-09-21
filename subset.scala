3064b5a27d17732e5918112d101cdb25674c500f
kim-tang2 <rlaxogns4504@gmail.com>
2020-09-21 23:56:47 +0900

    scala API subset

--- subset.scala
+++ subset.scala
@@ -6,8 +6,8 @@ object AllSubsets extends App{
     else subsets(xs.tail)
       .flatMap(ys => List(ys, xs.head :: ys))
 
-//  val ys = subsets(xs.tail)
-//  ys ++ ys.map { y => xs.head :: y }
+  //  val ys = subsets(xs.tail)
+  //  ys ++ ys.map { y => xs.head :: y }
 
   psubsets(List())
   psubsets(List(1))
@@ -17,5 +17,16 @@ object AllSubsets extends App{
   private def psubsets[T](xs: List[T]) {
     println(s"subsets($xs) =="
       + subsets(xs).mkString("List(\n ", "\n ", "\n)"))
+  }
+}
+
+object ScalaSubSets extends App{
+  psubsets(List())
+  psubsets(List(1))
+  psubsets(List(1, 2))
+
+  private def psubsets[T](xs: List[T]): Unit ={
+    println(s"subsets($xs) == "
+      + xs.toSet.subsets().mkString("List(\n ", "\n ", "\n)"))
   }
 }
