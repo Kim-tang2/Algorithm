c6a56333e263d7437547da1b95f4542ca67fd980
kim-tang2 <rlaxogns4504@gmail.com>
2020-09-21 23:50:12 +0900

    subset

---
+++ subset.scala
@@ -0,0 +1,21 @@
+package fpcoding.e08
+
+object AllSubsets extends App{
+  def subsets[T](xs: List[T]) : List[List[T]] =
+    if(xs.isEmpty) List(Nil)
+    else subsets(xs.tail)
+      .flatMap(ys => List(ys, xs.head :: ys))
+
+//  val ys = subsets(xs.tail)
+//  ys ++ ys.map { y => xs.head :: y }
+
+  psubsets(List())
+  psubsets(List(1))
+  psubsets(List(1, 2))
+  psubsets(List("a", "b", "c"))
+
+  private def psubsets[T](xs: List[T]) {
+    println(s"subsets($xs) =="
+      + subsets(xs).mkString("List(\n ", "\n ", "\n)"))
+  }
+}
