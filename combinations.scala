d98a31deb63fd9c1e8849448841da32fe3ec4167
kim-tang2 <rlaxogns4504@gmail.com>
2020-09-24 18:21:20 +0900

    combinations

---
+++ combinations.scala
@@ -0,0 +1,15 @@
+package fpcoding.e10
+
+object Combinations extends App {
+
+  def combinations[T](xs: List[T], k: Int): List[List[T]] =
+    if (k == 0) List(Nil)
+    else if (xs.isEmpty) Nil
+    else (
+      combinations(xs.tail, k - 1).map(xs.head :: _)
+        ++ combinations(xs.tail, k)
+      )
+
+  println(combinations(List(1, 2, 3), 2))
+  println(combinations(List(1, 2, 3, 4, 5), 3))
+}
