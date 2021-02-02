bb2caeb002a1c43f5aed2a606e2d8c6dd20797c3
taehoonkim <rlaxogns4504@gmail.com>
2021-02-03 04:13:31 +0900

    백준 11720 - 숫자의 합2

--- 백준 단계별로 풀어보기/[문자열] 백준 11720 - 숫자의 합.py
+++ 백준 단계별로 풀어보기/[문자열] 백준 11720 - 숫자의 합.py
@@ -2,10 +2,12 @@ import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 
-T = int(input()) # 의미 없음
-numbers = input()
-answer = 0
-for number in numbers:
-    answer += int(number)
+input()
+print(sum(map(int, input())))
 
-print(answer)
+# input() # 의미 없음
+# numbers = input()
+# answer = 0
+# for number in numbers:
+#     answer += int(number)
+
