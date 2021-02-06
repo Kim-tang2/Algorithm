9937481ffdd82a87c0f6f7185182c7b300cd2903
taehoonkim <rlaxogns4504@gmail.com>
2021-02-07 00:55:10 +0900

    백준 1157 - 단어 공부

--- 백준 단계별로 풀어보기/[문자열] 백준 1157 - 단어 공부.py
+++ 백준 단계별로 풀어보기/[문자열] 백준 1157 - 단어 공부.py
@@ -1,26 +1,15 @@
 import sys
 import string
-import operator
 
 input = lambda: sys.stdin.readline().rstrip()
 words = input().upper()
+rank_word = []
 
-if len(words) != 1:
-    not_overlap_word = []
-    for word in words:
-        if word not in not_overlap_word:
-            not_overlap_word.append(word)
+for alphabet in string.ascii_uppercase:
+    rank_word.append(words.count(alphabet))
 
-    words_dict = {}
-
-    for alphabet in not_overlap_word:
-        words_dict[alphabet] = words.count(alphabet)
-
-    sorted_dict = sorted(words_dict.items(), key=operator.itemgetter(1), reverse=True)
-
-    if sorted_dict[0][1] != sorted_dict[1][1]:
-        print(sorted_dict[0][0])
-    else:
-        print("?")
+if rank_word.count(max(rank_word)) > 1:
+    print("?")
 else:
-    print(words)
+    print(string.ascii_uppercase[rank_word.index(max(rank_word))])
+
