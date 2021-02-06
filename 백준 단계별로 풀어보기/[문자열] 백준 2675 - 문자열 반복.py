c251eebb9775a761fe4fcb5b6ab54f12217a195e
taehoonkim <rlaxogns4504@gmail.com>
2021-02-06 23:38:23 +0900

    백준 2675 - 문자열 반복

--- 백준 단계별로 풀어보기/[문자열] 백준 2675 - 문자열 반복.py
+++ 백준 단계별로 풀어보기/[문자열] 백준 2675 - 문자열 반복.py
@@ -4,9 +4,9 @@ input = lambda: sys.stdin.readline().rstrip()
 T = int(input())
 
 while T != 0:
-    repeatNum, *repeatStr = input()
+    repeatNum, repeatStr = input().split()
     for str in repeatStr:
-        print(str.lstrip() * int(repeatNum), end="")
+        print(str * int(repeatNum), end="")
     print()
 
     T -= 1
